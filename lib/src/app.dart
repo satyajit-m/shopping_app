5a941edde9cfe99c79a5464dfbd5450e791866bc
raffleberry <raffleberry42@gmail.com>
2019-04-18 16:50:04 +0530

    Added nav persistance

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,31 @@
-import 'package:flutter/material.dart';
+/*
+add badge when backend is implemented
+https://stackoverflow.com/questions/45155104/displaying-notification-badge-on-bottomnavigationbars-icon
+*/
 
-class MyApp extends StatelessWidget {
-  @override
+import 'package:flutter/material.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+
+import 'screens/home.dart';
+import 'screens/profile.dart';
+import 'screens/help.dart';
+
+class App extends StatefulWidget {
+  AppState createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int currentTab = 0;
+  final List<Widget> screens = [
+    HomeScreen(),
+    HelpScreen(),
+    ProfileScreen(),
+  ];
+  Widget currentScreen = HomeScreen();
+  final PageStorageBucket bucket = PageStorageBucket();
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shopping',
@@ -9,8 +33,31 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Shopping'),
         ),
-        body: Center(
-          child: Text('Hello There'),
+        body: PageStorage(
+          child: currentScreen,
+          bucket: bucket,
+        ),
+        bottomNavigationBar: BottomNavyBar(
+          currentIndex: currentTab,
+          onItemSelected: (index) => setState(() {
+                currentTab = index;
+                currentScreen = screens[currentTab];
+              }),
+          items: [
+            BottomNavyBarItem(
+              icon: Icon(Icons.apps),
+              title: Text('Home'),
+              activeColor: Colors.red,
+            ),
+            BottomNavyBarItem(
+              icon: Icon(Icons.help),
+              title: Text('Help'),
+              activeColor: Colors.purpleAccent),
+            BottomNavyBarItem(
+              icon: Icon(Icons.people),
+              title: Text('Account'),
+              activeColor: Colors.greenAccent),
+          ],
         ),
       ),
     );
---
+++ lib/src/screens/profile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+ ProfileScreenState createState() => ProfileScreenState();
+}
+
+class ProfileScreenState extends State<ProfileScreen> {
+
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.blue,
+        padding: EdgeInsets.all(50.0),
+        child: Text('Profile', style: TextStyle(color: Colors.white, fontSize: 48.0)),
+      ),
+    );
+  }
+}
