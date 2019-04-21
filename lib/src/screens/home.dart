07086a912aab1127af38911da20800a6e02ce5f0
raffleberry <raffleberry42@gmail.com>
2019-04-22 00:47:48 +0530

    navbar updated

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -6,13 +6,16 @@ class HomeScreen extends StatefulWidget {
 
 class HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.blue,
-        padding: EdgeInsets.all(50.0),
-        child:
-            Text('Home', style: TextStyle(color: Colors.white, fontSize: 48.0)),
-      ),
+    return (ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(20.0),
+          children: <Widget>[
+            const Text('I\'m dedicating every day to you'),
+            const Text('Domestic life was never quite my style'),
+            const Text('When you smile, you knock me out, I fall apart'),
+            const Text('And I thought I was so smart'),
+          ],
+        )
     );
   }
 }
