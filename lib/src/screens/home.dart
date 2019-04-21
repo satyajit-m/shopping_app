aa06bb6af445465d2fddfd6b9e94f1a8f6697af2
Satyajit Mohapatra <smohapatra200@gmail.com>
2019-04-22 01:12:54 +0530

    Update home.dart

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -7,14 +7,15 @@ class HomeScreen extends StatefulWidget {
 class HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return (ListView(
-      shrinkWrap: true,
-      padding: const EdgeInsets.all(20.0),
-      children: <Widget>[
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('And I thought I was so smart'),
-      ],
-    ));
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(20.0),
+          children: <Widget>[
+            const Text('I\'m dedicating every day to you'),
+            const Text('Domestic life was never quite my style'),
+            const Text('When you smile, you knock me out, I fall apart'),
+            const Text('And I thought I was so smart'),
+          ],
+        )
+    );
   }
 }
