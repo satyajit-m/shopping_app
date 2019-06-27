34e027cbfe8a908fb068a63e5b4d808346ded67a
raffleberry <raffleberry42@gmail.com>
2019-06-27 22:13:20 +0530

    services card layout added.

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -26,6 +26,7 @@ class HomeScreenState extends State<HomeScreen> {
   List<Widget> services = [];
 
   HomeScreenState() {
+    //implement firestore here.
     //static list
     List<String> fetchList = [
       'Home Appliances',
@@ -40,8 +41,19 @@ class HomeScreenState extends State<HomeScreen> {
       'Others',
     ];
 
-    for (int i = 0; i < fetchList.length; i++) {
-      services.add(CategoryCard(fetchList[i]));
+    if (fetchList.length % 2 != 0) {
+      fetchList.add('');
+    }
+
+    for (int i = 0; i < fetchList.length; i += 2) {
+      services.add(
+        Row(
+          children: <Widget>[
+            Expanded(child: Column(children: <Widget>[CategoryCard(fetchList[i])])),
+            Expanded(child: Column(children: <Widget>[CategoryCard(fetchList[i+1])])),
+          ],
+        )
+      );
     }
   }
 
@@ -77,69 +89,72 @@ class HomeScreenState extends State<HomeScreen> {
       },
       body: Container(
         child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            carouselSlider = CarouselSlider(
-              height: MediaQuery.of(context).size.height * 0.2,
-              initialPage: 0,
-              enlargeCenterPage: true,
-              autoPlay: true,
-              reverse: false,
-              enableInfiniteScroll: true,
-              autoPlayInterval: Duration(seconds: 1),
-              autoPlayAnimationDuration: Duration(milliseconds: 50000),
-              pauseAutoPlayOnTouch: Duration(seconds: 10),
-              scrollDirection: Axis.horizontal,
-              onPageChanged: (index) {
-                setState(() {
-                  _current = index;
-                });
-              },
-              items: imgList.map((imgUrl) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.symmetric(horizontal: 10.0),
-                      decoration: BoxDecoration(
-                        color: Colors.green,
-                      ),
-                      child: Image.network(
-                        imgUrl,
-                        fit: BoxFit.fill,
-                      ),
-                    );
-                  },
-                );
-              }).toList(),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: map<Widget>(imgList, (index, url) {
-                return Container(
-                  width: 10.0,
-                  height: 10.0,
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: _current == index ? Colors.redAccent : Colors.green,
-                  ),
-                );
-              }),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Column(
-              children: services,
-            )
-          ],
-        ),),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              carouselSlider = CarouselSlider(
+                height: MediaQuery.of(context).size.height * 0.2,
+                initialPage: 0,
+                enlargeCenterPage: true,
+                autoPlay: true,
+                reverse: false,
+                enableInfiniteScroll: true,
+                autoPlayInterval: Duration(seconds: 1),
+                autoPlayAnimationDuration: Duration(milliseconds: 50000),
+                pauseAutoPlayOnTouch: Duration(seconds: 10),
+                scrollDirection: Axis.horizontal,
+                onPageChanged: (index) {
+                  setState(() {
+                    _current = index;
+                  });
+                },
+                items: imgList.map((imgUrl) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 10.0),
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                        ),
+                        child: Image.network(
+                          imgUrl,
+                          fit: BoxFit.fill,
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: map<Widget>(imgList, (index, url) {
+                  return Container(
+                    width: 10.0,
+                    height: 10.0,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color:
+                          _current == index ? Colors.redAccent : Colors.green,
+                    ),
+                  );
+                }),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Column(
+                children: services,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
