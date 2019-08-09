941656caab55aefb94ee0e8b1949405f84824bb0
raffleberry <raffleberry42@gmail.com>
2019-08-09 19:15:05 +0530

    profile model

---
+++ lib/src/models/profile_model.dart
@@ -0,0 +1,12 @@
+class Profile {
+  final name;
+  final phone;
+  final pinCode;
+  final areaAndStreet;
+  final locality;
+  final landmark;
+  final altPhone;
+  Profile(this.name, this.phone, this.pinCode,
+          this.areaAndStreet, this.locality, this.landmark,
+          this.altPhone);
+}
