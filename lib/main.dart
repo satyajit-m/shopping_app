78105b2e2429478fded4d39e72d95decf11a8834
raffleberry <raffleberry42@gmail.com>
2019-08-12 02:31:26 +0530

    removed global Firebase User

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
-import 'auth/auth_page.dart';
 import 'package:shopping_app/src/app.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import 'auth/auth_page.dart';
+
+import 'src/forms/profile_form.dart';
+
 class MyApp extends StatelessWidget {
-
-  final FirebaseUser user;
-
-  MyApp(this.user);
-
   // MyApp() {
   //   //Navigation.initPaths();
   // }
@@ -22,14 +20,14 @@ class MyApp extends StatelessWidget {
       //onGenerateRoute: Navigation.router.generator,
       debugShowCheckedModeBanner: false,
       routes: {
-        '/': (context) => App(),
-        '/home': (context) => user == null ? AuthPage() : App(),
+        '/': (context) => AuthPage(),
+        '/home': (context) => App(),
+        '/profile/form': (context) => ProfileForm(),
       },
     );
   }
 }
 
-void main() async {
-  FirebaseUser user = await FirebaseAuth.instance.currentUser();
-  runApp(MyApp(user));
+void main() {
+  runApp(MyApp());
 }
