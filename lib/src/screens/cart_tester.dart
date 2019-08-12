15767a7c25e3aaf50fa6ce86877681ec8ca6cf12
raffleberry <raffleberry42@gmail.com>
2019-08-12 18:28:29 +0530

    For testing purpose

---
+++ lib/src/screens/cart_tester.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'cart.dart';
+
+import '../models/cart_model.dart';
+
+class Tester extends StatefulWidget {
+  final FirebaseUser user;
+  Tester({Key key, @required this.user});
+
+  createState() {
+    return TesterState();
+  }
+}
+
+class TesterState extends State<Tester> {
+
+  Widget build(BuildContext context) {
+    return SafeArea(child:Scaffold(
+      body: Center(
+        child: RaisedButton(
+          child: Text("OpenCart"),
+          onPressed: () {
+            final name = "Bull Milk";
+            final price = "699";
+            final serviceId = "78";
+            final x = Service(name, price, serviceId);
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => Cart(
+                  service: x,
+                  user: widget.user,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    ),);
+  }
+}
