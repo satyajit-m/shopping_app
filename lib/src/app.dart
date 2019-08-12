a2e19eab921eb85b19d93c396aef5c92fd959042
raffleberry <raffleberry42@gmail.com>
2019-08-12 18:26:50 +0530

    routes fixed

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,12 +3,16 @@ add badge when backend is implemented
 https://stackoverflow.com/questions/45155104/displaying-notification-badge-on-bottomnavigationbars-icon
 */
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/home.dart';
 //import 'screens/profile.dart';
+import './screens/cart_tester.dart';
 import 'screens/help.dart';
 import 'package:bmnav/bmnav.dart' as bmnav;
+
+import 'widgets/cube_grid.dart';
 
 class App extends StatefulWidget {
   AppState createState() {
@@ -17,38 +21,66 @@ class App extends StatefulWidget {
 }
 
 class AppState extends State<App> {
+  FirebaseUser user;
+  var dataLoaded = false;
+  AppState() {
+    getUser();
+  }
   int currentTab = 0;
-  final List<Widget> screens = [
-    HomeScreen(),
-    HelpScreen(),
-//    ProfileScreen(),
-  ];
+  List<Widget> screens;
   Widget currentScreen = HomeScreen();
   final PageStorageBucket bucket = PageStorageBucket();
 
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shopping',
-      home: Scaffold(
-        body: PageStorage(
-          child: currentScreen,
-          bucket: bucket,
+    if (!dataLoaded) {
+      return SafeArea(
+        child: Scaffold(
+          extendBody: true,
+          body: Center(
+            child: CubeGrid(
+              color: Colors.orange,
+              size: MediaQuery.of(context).size.width * 0.90,
+            ),
+          ),
         ),
-        bottomNavigationBar: bmnav.BottomNav(
-          onTap: (index) {
-            setState(() {
-              currentTab = index;
-              currentScreen = screens[index];
-            });
-          },
-          items: [
-            bmnav.BottomNavItem(Icons.home, label: 'Home'),
-            bmnav.BottomNavItem(Icons.location_on, label: 'Location'),
-            bmnav.BottomNavItem(Icons.person, label: 'profile'),
-          ],
-          iconStyle: bmnav.IconStyle(onSelectSize: 30.0),
-        ),
+      );
+    }
+
+    return SafeArea(child:Scaffold(
+      body: PageStorage(
+        child: currentScreen,
+        bucket: bucket,
       ),
-    );
+      bottomNavigationBar: bmnav.BottomNav(
+        onTap: (index) {
+          setState(() {
+            currentTab = index;
+            currentScreen = screens[index];
+          });
+        },
+        items: [
+          bmnav.BottomNavItem(Icons.home, label: 'Home'),
+          bmnav.BottomNavItem(Icons.location_on, label: 'Location'),
+          bmnav.BottomNavItem(Icons.person, label: 'profile'),
+        ],
+        iconStyle: bmnav.IconStyle(onSelectSize: 30.0),
+      ),
+    ),);
+  }
+
+  void getUser() async {
+    print("tryin to get Current User");
+    user = await FirebaseAuth.instance.currentUser();
+    screens = [
+      HomeScreen(),
+      HelpScreen(),
+      //    ProfileScreen(),
+      Tester(
+        user: user,
+      )
+    ];
+    setState(() {
+      dataLoaded = true;
+    });
   }
 }
